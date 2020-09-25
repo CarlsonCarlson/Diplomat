439989147e1b281f0319e6255697ac72cbe51c7e
Patrick Du <dupatrick@gmail.com>
2020-09-25 18:37:06 -0400

    fix merge issues

--- mediatorapp/src/main/java/com/sbuhack/mediatorapp/respository/MediatorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sbuhack.mediatorapp.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sbuhack.mediatorapp.entity.Mediator;
-
-public interface MediatorRepository extends JpaRepository<Mediator,String>{
-
-}
