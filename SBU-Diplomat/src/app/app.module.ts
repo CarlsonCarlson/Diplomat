b2d4809e33da1f23db54471afb9588e15f0b5fb8
Patrick Du <dupatrick@gmail.com>
2020-09-26 15:30:47 -0400

    created login

--- SBU-Diplomat/src/app/app.module.ts
+++ SBU-Diplomat/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -48,6 +48,7 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component:HomeComponent },
       { path: 'login', component: LoginComponent},
