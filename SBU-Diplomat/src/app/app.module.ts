aa2b106b3cca7ff2baba787e00b0d524f17d8da1
topheryun <topheryun@gmail.com>
2020-09-25 17:02:50 -0700

    added a bunch of components and basic nav bar

--- SBU-Diplomat/src/app/app.module.ts
+++ SBU-Diplomat/src/app/app.module.ts
@@ -6,15 +6,39 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 
+import { RouterModule } from '@angular/router';
+import { TempComponent } from './temp/temp.component';
+import { Form1Component } from './form1/form1.component';
+import { Form2Component } from './form2/form2.component';
+import { User1Component } from './user1/user1.component';
+import { User2Component } from './user2/user2.component';
+import { MediatorComponent } from './mediator/mediator.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    LoginComponent
+    LoginComponent,
+    TempComponent,
+    Form1Component,
+    Form2Component,
+    User1Component,
+    User2Component,
+    MediatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent},
+      { path: 'temp', component: TempComponent},
+      { path: 'form1', component: Form1Component},
+      { path: 'form2', component: Form2Component},
+      { path: 'user1', component: User1Component},
+      { path: 'user2', component: User2Component},
+      { path: 'mediator', component: MediatorComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
