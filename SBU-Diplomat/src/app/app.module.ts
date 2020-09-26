4014a79d23bf46a1b908838962bcadfd8303bb6f
topheryun <topheryun@gmail.com>
2020-09-25 19:27:29 -0700

    form1 sorta complete

--- SBU-Diplomat/src/app/app.module.ts
+++ SBU-Diplomat/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { MediatorComponent } from './mediator/mediator.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/login', pathMatch: 'full' },
--- SBU-Diplomat/src/app/form1/form1.component.ts
+++ SBU-Diplomat/src/app/form1/form1.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Form1 } from '../form1';
+
 @Component({
   selector: 'app-form1',
   templateUrl: './form1.component.html',
   styleUrls: ['./form1.component.css']
 })
 export class Form1Component implements OnInit {
+
+  yesNo = ['', 'Yes', 'No'];
+
+  model = new Form1(1, '', '', '', '', '', this.yesNo[0]);
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
 
   constructor() { }
 
